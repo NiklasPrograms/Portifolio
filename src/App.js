c6c2a7dcac34f721442653a83c796f366f1e0c09
Niklas Aavad <202008561@post.au.dk>
2022-02-02 22:41:00 +0100

    finished first react bootstrap tutorial

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 import './App.css';
-import { Button, Alert, Breadcrumb, Card, Form } from 'react-bootstrap'
+import { Container, Row, Col, Button, Alert, Breadcrumb, Card, Form } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Container>
         <h1>Velkommen</h1>
         <Form>
-          <Form.Group controlId="formEmail">
-            <Form.Label>Email Address</Form.Label>
-            <Form.Control type="email" placeholder="Example@email.com"/>
-            <Form.Text className="text-muted">
-              We'll never share your email address, trust us!
-            </Form.Text>
-          </Form.Group>
-          <Form.Group controlId="formPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password"/>
-            <Button className="mb-4 mt-2" variant="secondary" type="submit">Login</Button>
-          </Form.Group>
+          <Row>
+            <Col md> 
+              <Form.Group controlId="formEmail">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control type="email" placeholder="Example@email.com"/>
+                <Form.Text className="text-muted">
+                  We'll never share your email address, trust us!
+                </Form.Text>
+              </Form.Group>
+            </Col>
+            <Col md>
+            <Form.Group controlId="formPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password"/>
+            </Form.Group>
+            </Col>
+          </Row>
+          <Button className="mb-4 mt-2" variant="secondary" type="submit">Login</Button>
         </Form>
 
         <Card className="mb-3" style={{color: "#000"}}>
-          <Card.Img src="https://picsum.photos/200/100"/>
+          <Card.Img src="https://picsum.photos/900/400"/>
           <Card.Body>
             <Card.Title>
               Card Example
@@ -41,6 +48,7 @@ function App() {
         </Breadcrumb>
         <Alert variant="success">Below is a button</Alert>
         <Button className="mb-3">Test Button</Button>
+        </Container>
       </header>
     </div>
   );
