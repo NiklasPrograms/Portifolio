da0e0a4459f96314cb1eb85cfdf02d50ec25e7f8
Niklas Aavad <202008561@post.au.dk>
2022-02-02 21:30:50 +0100

    First part of react bootstrap tutorial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Alert, Breadcrumb, Card, Form } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>Velkommen</h1>
+        <Form>
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type="email" placeholder="Example@email.com"/>
+            <Form.Text className="text-muted">
+              We'll never share your email address, trust us!
+            </Form.Text>
+          </Form.Group>
+          <Form.Group controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password"/>
+            <Button className="mb-4 mt-2" variant="secondary" type="submit">Login</Button>
+          </Form.Group>
+        </Form>
+
+        <Card className="mb-3" style={{color: "#000"}}>
+          <Card.Img src="https://picsum.photos/200/100"/>
+          <Card.Body>
+            <Card.Title>
+              Card Example
+            </Card.Title>
+            <Card.Text>
+              This is an example of react bootstrap cards.
+            </Card.Text>
+            <Button variant="primary">Read More</Button>
+          </Card.Body>
+        </Card>
+        <Breadcrumb variant="secondary">
+          <Breadcrumb.Item>Test 1</Breadcrumb.Item>
+          <Breadcrumb.Item active>Test 2</Breadcrumb.Item>
+          <Breadcrumb.Item>Test 3</Breadcrumb.Item>
+        </Breadcrumb>
+        <Alert variant="success">Below is a button</Alert>
+        <Button className="mb-3">Test Button</Button>
       </header>
     </div>
   );
