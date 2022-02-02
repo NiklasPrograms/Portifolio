edc88c3e61526709f8781dfe25035951690ac207
Niklas Aavad <202008561@post.au.dk>
2022-02-03 00:21:57 +0100

    Created file for NavTemplate

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
-import { Navbar, Container } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home">
-        Niklas Aavad
-        </Navbar.Brand>
-      </Container>
-    </Navbar>
+    <div className='App'>
+      <p>
+        Lorem ipsum dolor sit amet, nam te munere cotidieque, cu odio aliquip phaedrum pri, idque laoreet vel ut. Mel civibus deleniti temporibus ea. No nam prima consequuntur. Per ut probo mucius recteque. An posse aeterno mel, assum erant sonet quo ex.
+        Dicat postulant ad per, dico nulla propriae cu usu, ea duo magna mandamus. Has suas idque an, at soluta delicata eam. At ius sumo nusquam efficiendi. Sale iriure concludaturque ad his. Id sed dicta verear, quidam tamquam abhorreant ut sit. Utroque accumsan eos ex, cum ferri vulputate ne.
+      </p>
+      <p>
+        Cetero utroque eum ex. Case adipisci expetenda et mel, est an legere commodo tractatos, cu iuvaret splendide dissentias mea. Omnis fugit assueverit his no, scaevola apeirian has ei, eu alii conclusionemque eos. Rationibus conclusionemque eam ne, qui clita maiorum eu.
+        Civibus concludaturque cu mei. At est primis scripserit, patrioque reformidans te his, vix aeterno nostrud et. Vel sadipscing persequeris ut, ea mea ornatus inimicus temporibus. Eam id exerci pericula.
+      </p>
+      <p>
+        Ea ius feugiat epicuri molestiae. Has impetus gloriatur ex, an dicat erant commune vim. Et qui nulla tibique oporteat, in habeo adhuc dissentiet eum. His persius appareat eu.
+      </p>
+    </div>
   );
 }
 
---
+++ src/NavTemplate.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { Nav, Navbar, Container } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const NavTemplate = () => {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+        Niklas Aavad
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavTemplate;
