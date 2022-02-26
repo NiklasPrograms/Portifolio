2e8b5231b0475f568f2358dd937a42ef698c88a7
Niklas <niklasaavad@gmail.com>
2022-02-26 20:54:24 +0100

    added some lorem ipsum to the frontpage

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import PortraitCard from './PortraitCard';
 
 function App() {
   return (
-    <Container  lassName='App'>
+    <Container>
       <Row>
         <Col>
           Lorem ipsum dolor sit amet, nam te munere cotidieque, cu odio aliquip phaedrum pri, idque laoreet vel ut. Mel civibus deleniti temporibus ea. No nam prima consequuntur. Per ut probo mucius recteque. An posse aeterno mel, assum erant sonet quo ex.
@@ -18,13 +18,12 @@ function App() {
           Cetero utroque eum ex. Case adipisci expetenda et mel, est an legere commodo tractatos, cu iuvaret splendide dissentias mea. Omnis fugit assueverit his no, scaevola apeirian has ei, eu alii conclusionemque eos. Rationibus conclusionemque eam ne, qui clita maiorum eu.
           Civibus concludaturque cu mei. At est primis scripserit, patrioque reformidans te his, vix aeterno nostrud et. Vel sadipscing persequeris ut, ea mea ornatus inimicus temporibus. Eam id exerci pericula.
           Ea ius feugiat epicuri molestiae. Has impetus gloriatur ex, an dicat erant commune vim. Et qui nulla tibique oporteat, in habeo adhuc dissentiet eum. His persius appareat eu.
+          Donec a elementum mi, ac dictum risus. Sed pulvinar gravida augue eu mattis. Cras felis lectus, fringilla a ornare vel, sollicitudin quis leo. Nunc vehicula elementum lorem sit amet condimentum. In massa leo, rhoncus sit amet condimentum ac, hendrerit nec mauris. Aenean faucibus neque nec tortor euismod hendrerit. In bibendum hendrerit blandit. Pellentesque finibus pellentesque euismod. Nullam tortor ipsum, dapibus eu accumsan sed, ornare eget odio. Nam sollicitudin enim nulla. Curabitur justo ligula, venenatis ut facilisis interdum, laoreet ac ex.
+          Nulla urna libero, efficitur sit amet nisi a, mattis imperdiet est. Nulla ac aliquam eros. Proin posuere nibh ac nulla hendrerit luctus. Pellentesque finibus bibendum dui finibus imperdiet. Integer vestibulum, risus non consequat sollicitudin, lorem est pretium orci, porta sollicitudin dui felis et turpis. Nunc venenatis nulla elit, a tempor lectus tincidunt et.  
         </Col>
-        <Col>
+        <Col lg="4">
           <PortraitCard />
         </Col>
-      </Row>
-      <Row>
-        
       </Row>
     </Container>
   );
